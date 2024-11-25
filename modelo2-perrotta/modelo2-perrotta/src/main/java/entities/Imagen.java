8d3b14642e99f055f3307f24b15d2cb38e204963
biancaperrotta2 <biancaperrotta8@gmail.com>
2024-11-25 19:38:54 -0300

    Add files via upload

---
+++ modelo2-perrotta/modelo2-perrotta/src/main/java/entities/Imagen.java
@@ -0,0 +1,14 @@
+package entities;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+@Setter
+@Builder
+public class Imagen {
+    private Long id;
+    private String denominacion;
+}
---
+++ modelo2-perrotta/modelo2-perrotta/src/main/java/entities/UnidadMedida.java
@@ -0,0 +1,15 @@
+package entities;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+@Setter
+@Builder
+public class
+UnidadMedida {
+    private Long id;
+    private String denominacion;
+}
